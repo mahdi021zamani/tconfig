d78dc9a6a16e4417fc9bd56c40cc405fe939ed18
mahdi021zamani <167538602+mahdi021zamani@users.noreply.github.com>
2025-04-07 23:02:20 +0330

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,6 +110,9 @@ $(document).on('click', '#get', function(e) {
         if ( type === "warp" ) {
             config = 'https://mahdi-ircf-ww.mahdi021zamani.workers.dev/';
         }
+        if ( type === "shakhsi" ) {
+            config = 'https://vfarid2-3.mahdi021zamani.workers.dev/sub#sub';
+        }
         else if ( type === "mix" ) {
             config = 'https://raw.githubusercontent.com/'+source+'/main/splitted/mixed';
         }
